e04ebd9d0c834e564e3e030139c2985efa1aa884
Alistair Miles <alimanfoo@googlemail.com>
2024-10-10 09:30:49 +0100

    checkpoint minor acceleration

--- anjl/_rapid.py
+++ anjl/_rapid.py
@@ -205,7 +205,6 @@ def _rapid_iteration(
             nodes_sorted=nodes_sorted,
             clustered=clustered,
             obsolete=obsolete,
-            index_to_id=index_to_id,
             id_to_index=id_to_index,
             n_remaining=n_remaining,
             u_max=u_max,
@@ -301,7 +300,6 @@ def _rapid_search(
     clustered: np.ndarray,
     obsolete: np.ndarray,
     id_to_index: np.ndarray,
-    index_to_id: np.ndarray,
     n_remaining: int,
     u_max: np.float32,
     # ) -> tuple[int, int, int, int]:
@@ -311,37 +309,11 @@ def _rapid_search(
     threshold = numba.float32(np.inf)
     i_min = -1
     j_min = -1
-    # searched = 0
-    # visited = 0
     coefficient = numba.float32(n_remaining - 2)
     m = nodes_sorted.shape[0]
     n = nodes_sorted.shape[1]
     assert m == D_sorted.shape[0]
     assert n == D_sorted.shape[1]
-
-    # First pass, seed q_min and threshold with first in each row. This is suggested
-    # in the original paper, but I'm not sure it actually makes much if any difference
-    # in practice.
-    for i in range(m):
-        if obsolete[i]:
-            continue
-        u_i = U[i]
-        node_j = nodes_sorted[i, 0]
-        assert node_j >= 0
-        if clustered[node_j]:
-            continue
-        node_i = index_to_id[i]
-        if node_i == node_j:
-            continue
-        d = D_sorted[i, 0]
-        j = id_to_index[node_j]
-        u_j = U[j]
-        q = coefficient * d - u_i - u_j
-        if q < q_min:
-            q_min = q
-            threshold = q_min + u_max
-            i_min = i
-            j_min = j
 
     # Search all values up to threshold.
     for i in range(m):
@@ -352,23 +324,18 @@ def _rapid_search(
         # Obtain divergence for node corresponding to this row.
         u_i = U[i]
 
-        # Obtain identifier for node corresponding to the current row.
-        # node_i = index_to_id[i]
-
         # Search the row up to threshold.
-        for s in range(1, n):
-            # visited += 1
-
+        for s in range(0, n):
             # Obtain node identifier for the current item.
             node_j = nodes_sorted[i, s]
-
-            # Skip if this node is already clustered.
-            if clustered[node_j]:
-                continue
 
             # Break at end of nodes.
             if node_j < 0:
                 break
+
+            # Skip if this node is already clustered.
+            if clustered[node_j]:
+                continue
 
             # Access distance.
             d = D_sorted[i, s]
@@ -386,7 +353,6 @@ def _rapid_search(
             j = id_to_index[node_j]
             u_j = U[j]
             q = q_partial - u_j
-            # searched += 1
 
             if q < q_min:
                 q_min = q
@@ -394,7 +360,6 @@ def _rapid_search(
                 i_min = i
                 j_min = j
 
-    # return i_min, j_min, searched, visited
     return i_min, j_min
 
 
